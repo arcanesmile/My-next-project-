8ff1f9caa0e44800673bfac07cf23f8108e5153c
Bem Benjamin <arcanesmile999@gmail.com>
2025-03-31 21:09:35 +0100

    vommit

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -3,7 +3,7 @@ import BlogLayout from '@/components/BlogLayout';
 
 export default function Blog({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen">
+    <div >
       <BlogLayout />
       <main>{children}</main>
     </div>
